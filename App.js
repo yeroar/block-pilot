85f4c6f566e7aa90d2545ab3960be10db79bdcb0
Ye Roar <nyecloud@gmail.com>
2025-07-27 03:54:12 +0200

    textonly buttons tested

--- App.js
+++ App.js
@@ -1,22 +1,27 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
-import FWButton from "./cc-components/Button/FWButton";
+import CustomKeyButton from "./cc-components/KeyboardButton/KeyboardButton";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <FWButton
-        label="Default Button"
-        onPress={() => console.log("Default Button Pressed")}
-        state="default"
-        disabled={false}
-      />
-      <FWButton
-        label="Disabled Button"
-        onPress={() => console.log("Disabled Button Pressed")}
-        state="default"
-        disabled={true}
-      />
+      <View style={styles.keyboardRow}>
+        <CustomKeyButton
+          variant="textOnly"
+          label="1"
+          onPress={() => console.log("Key 1 pressed")}
+        />
+        <CustomKeyButton
+          variant="textOnly"
+          label="2"
+          onPress={() => console.log("Key 2 pressed")}
+        />
+        <CustomKeyButton
+          variant="textOnly"
+          label="3"
+          onPress={() => console.log("Key 3 pressed")}
+        />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -28,5 +33,11 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  keyboardRow: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    width: "80%",
+    marginVertical: 10,
   },
 });
